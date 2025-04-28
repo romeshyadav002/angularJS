fe3ba25d4e60b81895cc75f7fed0da77898e2522
romeshyadav002 <raojiromesh002@gmail.com>
2025-04-28 21:07:41 +0530

    toDo list

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 // app.js
 var app = angular.module('myApp', []);
 
-app.controller('MainController', function($scope) {
-  $scope.name = "World";
+app.controller('TodoController', function($scope) {
+  $scope.tasks = []; // list of tasks
+
+  $scope.addTask = function() {
+    if ($scope.newTask) {
+      $scope.tasks.push($scope.newTask);
+      $scope.newTask = '';
+    }
+  };
+
+  $scope.deleteTask = function(index) {
+    $scope.tasks.splice(index, 1); // remove 1 item at position 'index'
+  };
 });
+
